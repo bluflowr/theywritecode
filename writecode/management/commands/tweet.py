d68b3140ad545462da4f2c2e1b85dcfb644913db
Irma Kramer <bluflowr@gmail.com>
2016-09-12 09:43:05 -0500

    Update to remove 1 descriptor.

--- writecode/management/commands/tweet.py
+++ writecode/management/commands/tweet.py
@@ -1,23 +1,9 @@
 from django.core.management.base import BaseCommand, CommandError
 from writecode.models import Word
+from writecode.views import pronoun, article, random_sentence
 from secret import ckey, cseckey, access_token, access_token_sec, top_range, mid_range
 import random
 from twython import Twython
-
-def pronoun(number):
-    if number >= top_range:
-        return "He's "
-    elif number >= mid_range and number < top_range:
-        return "One's "
-    else:
-        return "She's "
-
-def article(word):
-    vowels = ['a','e', 'i', 'o', 'u']
-    if word[0] in vowels:
-        return "an "
-    else:
-        return "a "
 
 class Command(BaseCommand):
 
@@ -29,22 +15,9 @@ class Command(BaseCommand):
         OAUTH_TOKEN_SECRET = access_token_sec
         twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
-        twc = ''
-        while len(twc) < 1 or len(twc) > 140:
-            coders = list(Word.objects.filter(word_type = "C"))
-            adjs = list(Word.objects.filter(word_type = "A"))
-            phrases = list(Word.objects.filter(word_type = "P"))
-            rand_coder = random.sample(coders, 2)
-            rand_adj = random.sample(adjs, 4)
-            rand_phrase = random.sample(phrases, 2)
-            rand_pronoun = random.sample(range(1,101), 2)
+        sentence = ''
+        while len(sentence) < 1 or len(sentence) > 140:
+            sentence = random_sentence
+            sentence = sentence() + " #theywritecode"
 
-            twc = (pronoun(rand_pronoun[0]) + article(rand_adj[0].words) + rand_adj[0].words + " " + 
-                rand_adj[1].words + " " + rand_coder[0].words + " " + rand_phrase[0].words + " " + 
-                pronoun(rand_pronoun[1]) + article(rand_adj[2].words) + rand_adj[2].words + " " + rand_adj[3].words +
-                 " " + rand_coder[1].words + " " + rand_phrase[1].words + " #theywritecode")
-            print (twc + " is " + str(len(twc)) + " char long.")
-
-
-        print ("FINAL: " + twc + " is " + str(len(twc)) + " char long.")
-        twitter.update_status(status=twc)
+        twitter.update_status(status=sentence)
--- writecode/views.py
+++ writecode/views.py
@@ -4,6 +4,7 @@ from .models import Word
 from secret import top_range, mid_range
 import random
 
+#Function to randomize pronouns
 def pronoun(number):
     if number >= top_range:
         return "He's "
@@ -12,6 +13,7 @@ def pronoun(number):
     else:
         return "She's "
 
+#Function to get correct article for adjectives
 def article(word):
     vowels = ['a','e', 'i', 'o', 'u']
     if word[0] in vowels:
@@ -19,21 +21,24 @@ def article(word):
     else:
         return "a "
 
-
-def new_plot(request):
+#Function to create a random sentence
+def random_sentence(twc = ''):
     coders = list(Word.objects.filter(word_type = "C"))
     adjs = list(Word.objects.filter(word_type = "A"))
     phrases = list(Word.objects.filter(word_type = "P"))
     rand_coder = random.sample(coders, 2)
-    rand_adj = random.sample(adjs, 4)
+    rand_adj = random.sample(adjs, 2)
     rand_phrase = random.sample(phrases, 2)
     rand_pronoun = random.sample(range(1,101), 2)
 
-    sentence = (pronoun(rand_pronoun[0]) + article(rand_adj[0].words) + rand_adj[0].words + " " + 
-        rand_adj[1].words + " " + rand_coder[0].words + " " + rand_phrase[0].words + " " + 
-        pronoun(rand_pronoun[1]) + article(rand_adj[2].words) + rand_adj[2].words + " " + rand_adj[3].words +
+    twc = (pronoun(rand_pronoun[0]) + article(rand_adj[0].words) + rand_adj[0].words + " " + 
+        rand_coder[0].words + " " + rand_phrase[0].words + " " + 
+        pronoun(rand_pronoun[1]) + article(rand_adj[1].words) + rand_adj[1].words +
          " " + rand_coder[1].words + " " + rand_phrase[1].words)
-    
+    return twc 
 
+
+def new_plot(request):
+    sentence = random_sentence
     return render(request, 'index.html', {'sentence' : sentence})
 
